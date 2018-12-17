6b36449a98204f6165f53e95aa0d6a016fb97459
Hafeez <feeziman007@hotmail.com>
2018-12-18 06:12:27 +1100

    cypress E2E test added

--- webpack.common.js
+++ webpack.common.js
@@ -1,24 +1,25 @@
 module.exports = {
-    entry: './src/index.js',
-    output: {
-        path: __dirname + '/dist',
-        publicPath: '/',
-        filename: 'bundle.js'
-    },
-    devServer: {
-        contentBase: './dist',
-        hot: true
-    },
-    module: {
-        rules: [
-            {
-                test: /\.(js|jsx)$/,
-                exclude: /node_modules/,
-                use: ['babel-loader']
-            }
-        ]
-    },
-    resolve: {
-        extensions: ['*', '.js', '.jsx']
-    }
+  entry: "./src/index.js",
+  output: {
+    path: __dirname + "/dist",
+    publicPath: "/",
+    filename: "bundle.js"
+  },
+  devServer: {
+    port: 8070,
+    contentBase: "./dist",
+    hot: true
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: ["babel-loader"]
+      }
+    ]
+  },
+  resolve: {
+    extensions: ["*", ".js", ".jsx"]
+  }
 };
